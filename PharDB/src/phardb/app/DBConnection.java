daf08445b97f94c21e574c263470c1a313fe2c2c
apple <apple@192.168.1.32>
2018-03-19 21:19:40 -0400

    close db

--- PharDB/src/phardb/app/DBConnection.java
+++ PharDB/src/phardb/app/DBConnection.java
@@ -3,12 +3,26 @@ package phardb.app;
 import java.sql.Connection;
 import java.util.UUID;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Scanner;
 
 import phardb.exception.ConnectException;
+/*
+1. Add an employee
+2. Check the inventory of a product in a certain branch store by product_id.
+3. Restocking a product in a branch store. Insert a new restocking record in the restocking table, and update the inventory i.e. the "Has" table.
+4. Insert a new dispense record. Trigger the update in prescription table 
+5. Place an order. insert ordering record, and contains table. (parameters: product_id, amount, store_id, emplyee_id, member_id(can be null)
+6. Quit
+
+ */
+
 
 public class DBConnection {
 	private static Connection conn=null;
+	private static Statement stmt=null;
+	
 	
 	public static void psqlConnect() throws ConnectException{
 		
@@ -16,8 +30,18 @@ public class DBConnection {
 			Class.forName("org.postgresql.Driver");
 			conn=DriverManager.getConnection("jdbc:postgresql://comp421.cs.mcgill.ca:5432/cs421", 
 					"cs421g49", "7749-comp421");
+			stmt=conn.createStatement();
 		}catch(Exception e){
 			throw new ConnectException("Fail to Connect to Database: jdbc:postgresql://comp421.cs.mcgill.ca:5432/cs421");
+		}
+	}
+	
+	public static void psqlClose() throws SQLException{
+		try {
+			stmt.close();
+			conn.close();
+		} catch (SQLException e) {
+			throw e;
 		}
 	}
 	
@@ -29,11 +53,14 @@ public class DBConnection {
 			psqlConnect();
 			
 			while(true){
-				System.out.println("Choose one of the following options by type in the words stated in ()");
+				System.out.println("Choose one of the following options by type in option letter");
 				System.out.println();
 				
 				Scanner sc=new Scanner(System.in);
+				String opt=sc.nextLine();
 				
+				
+				psqlClose();
 			}
 			
 		}catch(Exception e){
