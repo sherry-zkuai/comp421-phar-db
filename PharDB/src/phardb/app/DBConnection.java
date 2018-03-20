e7f592e4f932abe3d815669d1da1cb9f4765c8be
zkuai <apple@192.168.1.32>
2018-03-19 20:57:00 -0400

    add UUID

--- PharDB/src/phardb/app/DBConnection.java
+++ PharDB/src/phardb/app/DBConnection.java
@@ -1,13 +1,17 @@
 package phardb.app;
 
 import java.sql.Connection;
+import java.util.UUID;
 import java.sql.DriverManager;
+import java.util.Scanner;
 
 import phardb.exception.ConnectException;
 
 public class DBConnection {
+	private static Connection conn=null;
+	
 	public static void psqlConnect() throws ConnectException{
-		Connection conn=null;
+		
 		try{
 			Class.forName("org.postgresql.Driver");
 			conn=DriverManager.getConnection("jdbc:postgresql://comp421.cs.mcgill.ca:5432/cs421", 
@@ -16,7 +20,28 @@ public class DBConnection {
 			throw new ConnectException("Fail to Connect to Database: jdbc:postgresql://comp421.cs.mcgill.ca:5432/cs421");
 		}
 	}
+	
+	//public static void
+	
+	
 	public static void main(String args[]){
-		//psqlConnect();
+//		try{
+//			psqlConnect();
+//			
+//			while(true){
+//				System.out.println("Choose one of the following options by type in the words stated in ()");
+//				System.out.println();
+//				
+//				Scanner sc=new Scanner(System.in);
+//				
+//			}
+//			
+//		}catch(Exception e){
+//			System.out.println(e.getMessage());
+//			System.exit(0);
+//		}
+		UUID id=UUID.randomUUID();
+		System.out.println(id.toString());
+		
 	}
 }
