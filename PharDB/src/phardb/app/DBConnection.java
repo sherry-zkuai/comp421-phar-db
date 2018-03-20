3900c1e45947ddd0a2101f6fdc91f77722f426a1
Apple <Apple@asavpn23350.vpn.mcgill.ca>
2018-03-19 21:16:37 -0400

    comm

--- PharDB/src/phardb/app/DBConnection.java
+++ PharDB/src/phardb/app/DBConnection.java
@@ -6,6 +6,16 @@ import java.sql.DriverManager;
 import java.util.Scanner;
 
 import phardb.exception.ConnectException;
+/*
+1. Add an employee
+2. Check the inventory of a product in a certain branch store by product_id.
+3. Restocking a product in a branch store. Insert a new restocking record in the restocking table, and update the inventory i.e. the "Has" table.
+4. Insert a new dispense record. Trigger the update in prescription table 
+5. Place an order. insert ordering record, and contains table. (parameters: product_id, amount, store_id, emplyee_id, member_id(can be null)
+6. Quit
+
+ */
+
 
 public class DBConnection {
 	private static Connection conn=null;
